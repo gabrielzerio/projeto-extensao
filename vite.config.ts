2a8308a7fc85b7e26987e993250366496b488f81
Gabriel Zério <21043296+gabrielzerio@users.noreply.github.com>
2025-04-25 10:29:59 -0300

    configurando vite e build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+// Corrige __dirname para ESM
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: '.', // raiz do projeto (ajuste se necessário)
   build: {
-    outDir: 'dist'
+    rollupOptions: {
+      input: {
+        // Corrige caminhos relativos
+        main: resolve(__dirname, 'index.html'),
+        game: resolve(__dirname, 'src/pages/game.html'),
+        // ...outras páginas
+      }
+    },
   }
 });
